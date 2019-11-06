bae2942855b52a6b32c972c86228c09afe9961ec
Ricky dildy <rdildy92@gmail.com>
2019-11-06 18:06:38 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,7 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts{"Freddy Mercury"}.collect |favorite_ice_cream_flavors|
+  contacts.collect |favorite_ice_cream_flavors|
   favorite_ice_cream_flavors >> strawberry
 
 end
